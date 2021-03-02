80613b66f25509c40c0ddc11f6ebf29121fe30a5
Pablo Menéndez ◔_◔ <pabloyo97@hotmail.com>
2021-03-02 20:47:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -16,45 +16,4 @@ window.onload = function(){
 
       });
 
-      function type(element,txt,speed){
-        var i = 0;
-        function typeWriter() {
-          if (i < txt.length) {
-            document.getElementById(element).innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-          }
-        }
-        typeWriter();
-      }
-
-      
-      setTimeout(()=>{
-        type('title','Hola Lilia',70);
-      },300)
-      
-
-      setTimeout(()=>{
-        type('p1','Hoy es mi cumpleaños :)',70);
-      },1500)
-
-      setTimeout(()=>{
-        type('p2','Solo quería decirte',70);
-      },3800)
-
-      setTimeout(()=>{
-        type('p3','...',200);
-      },5500)
-
-      setTimeout(()=>{
-        type('p4','Que ya no se como llamar tu atención',70);
-        setTimeout(()=>{
-          document.getElementById('leftEyebrow').style.display = 'inline';
-          document.getElementById('rightEyebrow').style.display = 'inline';
-          //document.getElementById('fire').style.visibility = 'visible';
-          document.getElementById('leftEyeIris').style.backgroundImage='radial-gradient(rgb(255,80,0) 20%, rgba(255,80,0,0) 100%)';
-          document.getElementById('rightEyeIris').style.backgroundImage='radial-gradient(rgb(255,80,0) 20%, rgba(255,80,0,0) 100%)';
-        },2800)
-      },7500)
-
     }
